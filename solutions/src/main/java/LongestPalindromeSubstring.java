8d750b1682deef3dde453b0b404fb7326a712f9b
petar-stojanovic <banakafala1a@gmail.com>
2024-11-22 16:54:42 +0100

    refactored LongestPalindromeSubstring

--- solutions/src/main/java/LongestPalindromeSubstring.java
+++ solutions/src/main/java/LongestPalindromeSubstring.java
@@ -3,43 +3,39 @@ public class LongestPalindromeSubstring {
     public String longestPalindrome(String s) {
         if (s == null || s.isEmpty()) return "";
 
-        String str = "";
-        int minLength = 0;
+        String longest = "";
 
         for (int i = 0; i < s.length(); i++) {
-            int left = i;
-            int right = i;
 
-            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            String oddPalindrome = expandFromCenter(s, i, i);
+            String evenPalindrome = expandFromCenter(s, i, i + 1);
 
-                if (right - left + 1 > minLength) {
-                    minLength = right - left + 1;
-                    str = s.substring(left, right + 1);
-                }
-                left--;
-                right++;
+            if (longest.length() < oddPalindrome.length()) {
+                longest = oddPalindrome;
             }
 
-            left = i;
-            right = i+1;
-
-            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-
-                if (right - left + 1 > minLength) {
-                    minLength = right - left + 1;
-                    str = s.substring(left, right + 1);
-                }
-                left--;
-                right++;
+            if (longest.length() < evenPalindrome.length()) {
+                longest = evenPalindrome;
             }
         }
 
-        return str;
+        return longest;
+    }
+
+    private String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
     }
 
     public static void main(String[] args) {
 
         LongestPalindromeSubstring palindrome = new LongestPalindromeSubstring();
         System.out.println("Longest Palindromic Substring " + palindrome.longestPalindrome("cbbd"));
+        System.out.println("Longest Palindromic Substring " + palindrome.longestPalindrome("anavolimilovana"));
+        System.out.println("Longest Palindromic Substring " + palindrome.longestPalindrome("babad"));
+        System.out.println("Longest Palindromic Substring " + palindrome.longestPalindrome("bb"));
     }
 }
